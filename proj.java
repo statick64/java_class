7b82e70c0aad58dd723854068bf284d22c4bcb42
statick64 <kennethisong@gmail.com>
2022-08-17 17:57:40 +0200

    second commit

---
+++ proj.java
@@ -0,0 +1,40 @@
+class proj{
+     // public static int multip(int number){
+    //     int plus = number + 1;
+    //     System.out.println(plus);
+    //     return 0;
+
+    // }
+    public static void main(String[] args){
+        
+        System.out.println("Hello World");
+        
+        
+        // int my_array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // int sum = 0;
+
+        // for (int i : my_array)
+        //     sum += i;
+        //     System.out.println("The sum is " + sum);
+    
+    
+    
+    //     int [] numbers = {3,9,6,1,2};
+    //     int sum = 0;
+
+    //   for (int i = 0; i < numbers.length; i++){
+    //     sum = numbers[i] + sum;
+    //     System.out.println(sum);
+        // multip(numbers[i]);
+    //   }
+    //   System.out.printf("The sum of the array is: %d", sum);
+        
+
+
+
+
+    }
+
+
+
+}
